5a2ddef7ce0cd425974ca32ad2952e37a426a50b
dagra27407 <danielwlambert@gmail.com>
2025-10-16 09:15:43 -0400

    chore: prune unused ui files

--- supabase/functions/waitlist/index.ts
+++ supabase/functions/waitlist/index.ts
@@ -3,7 +3,7 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 
 const cors = {
-  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Origin": "https://spinalith.com",
   "Access-Control-Allow-Methods": "POST, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type",
 };
